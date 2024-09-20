a6f0811a7041be14a341395cf08fa2a4e84e24d2
e.larroche <e.larroche@lockself.com>
2024-09-20 16:54:19 +0200

    config server

---
+++ server/models/client.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const ClientSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		startDate: {
+			type: Date,
+			required: true,
+		},
+		endDate: {
+			type: Date,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	},
+)
+
+module.exports = mongoose.model("Client", ClientSchema)
+
