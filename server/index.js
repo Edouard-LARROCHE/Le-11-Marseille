2ff746265269b97462a05ae9a07e7827750b4e88
e.larroche <e.larroche@lockself.com>
2024-09-20 16:43:38 +0200

    connect to mongo DB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express")
 const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const password = encodeURIComponent(process.env.VITE_PASSWORD_MGDB)
+const uri = `mongodb+srv://${process.env.VITE_USER_MGDB}:${password}@${process.env.VITE_CLUSTER_MGDB}/?retryWrites=true&w=majority&appName=${process.env.VITE_NAME_MGDB}`
 
 app.use(cors())
 app.use(express.json())
+
+mongoose
+	.connect(uri)
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.log(err))
 
 app.get("/api", (req, res) => {
 	res.json({ message: "Bonjour depuis le backend!" })
