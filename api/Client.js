209ef635b8a67dd2a09b8471a0b19bde4f732656
e.larroche <e.larroche@lockself.com>
2024-10-01 13:11:01 +0200

    update...

--- api/Client.js
+++ api/Client.js
@@ -1,40 +1,28 @@
 import mongoose from "mongoose"
 import Client from "../server/models/client"
 
-const password = encodeURIComponent(import.meta.env.VITE_PASSWORD_MGDB)
-const uri = `mongodb+srv://${import.meta.env.VITE_USER_MGDB}:${password}@${import.meta.env.VITE_CLUSTER_MGDB}/?retryWrites=true&w=majority&appName=${import.meta.env.VITE_NAME_MGDB}`
+const password = encodeURIComponent(process.env.VITE_PASSWORD_MGDB)
+const uri = `mongodb+srv://${process.env.VITE_USER_MGDB}:${password}@${process.env.VITE_CLUSTER_MGDB}/?retryWrites=true&w=majority&appName=${process.env.VITE_NAME_MGDB}`
 
 const connectMongo = async () => {
-	if (mongoose.connection.readyState === 0) {
-		await mongoose.connect(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
-	}
+	if (mongoose.connections[0].readyState) return
+
+	await mongoose.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 }
 
 export default async function handler(req, res) {
 	await connectMongo()
 
 	if (req.method === "GET") {
-		try {
-			const clients = await Client.find({})
-			res.status(200).json(clients)
-		} catch (error) {
-			res.status(500).json({
-				error: "Erreur lors de la récupération des clients",
-			})
-		}
+		const clients = await Client.find({})
+		res.status(200).json(clients)
 	} else if (req.method === "POST") {
-		try {
-			const client = new Client(req.body)
-			await client.save()
-			res.status(201).json(client)
-		} catch (error) {
-			res.status(500).json({
-				error: "Erreur lors de la création du client",
-			})
-		}
+		const client = new Client(req.body)
+		await client.save()
+		res.status(201).json(client)
 	} else {
 		res.status(405).json({ message: "Method not allowed" })
 	}
