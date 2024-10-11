c4885ceeb663c4d398162a93ec9d75163a3446d7
e.larroche <e.larroche@lockself.com>
2024-10-11 16:31:35 +0200

    update vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
+	build: {
+		target: "esnext",
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+			},
+		},
+	},
 	server: {
 		proxy: {
 			"/api": {
